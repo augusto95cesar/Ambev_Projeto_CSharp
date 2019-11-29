e82ca3aa8a40873bc2d0e9fc495efa20b4e91130
Augsuto Cesar <augusto95cesar@gmail.com>
2019-11-29 10:38:10 -0300

    Cadastro de Funcionario

---
+++ Funcionarios_Ambev/Models/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Funcionarios_Ambev.Models
+{
+    [Table("dbo.Funcioanrio")]
+    public class Funcionario
+    {
+        [Key,Column("FuncionarioId")]
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Idade { get; set; }
+    }
+}
